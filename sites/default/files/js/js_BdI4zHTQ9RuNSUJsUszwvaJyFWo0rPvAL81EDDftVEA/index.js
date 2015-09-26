c4156ccc2fe07ad75cd137bdb019c96b24715080
rho <ryan@theoleschool.com>
2015-09-26 12:08:47 -0500

    initial commit

---
+++ sites/default/files/js/js_BdI4zHTQ9RuNSUJsUszwvaJyFWo0rPvAL81EDDftVEA/index.js
@@ -0,0 +1,143 @@
+/**
+ * @file
+ * JavaScript for the Disqus Drupal module.
+ */
+
+// The Disqus global variables.
+var disqus_shortname = '';
+var disqus_url = '';
+var disqus_title = '';
+var disqus_identifier = '';
+var disqus_developer = 0;
+var disqus_def_name = '';
+var disqus_def_email = '';
+var disqus_config;
+
+(function ($) {
+
+/**
+ * Drupal Disqus behavior.
+ */
+Drupal.behaviors.disqus = {
+  attach: function (context, settings) {
+    $('body').once('disqus', function() {
+      // Load the Disqus comments.
+      if (settings.disqus || false) {
+        // Setup the global JavaScript variables for Disqus.
+        disqus_shortname = settings.disqus.domain;
+        disqus_url = settings.disqus.url;
+        disqus_title = settings.disqus.title;
+        disqus_identifier = settings.disqus.identifier;
+        disqus_developer = settings.disqus.developer || 0;
+        disqus_def_name = settings.disqus.name || '';
+        disqus_def_email = settings.disqus.email || '';
+
+        // Language and SSO settings are passed in through disqus_config().
+        disqus_config = function() {
+          if (settings.disqus.language || false) {
+            this.language = settings.disqus.language;
+          }
+          if (settings.disqus.remote_auth_s3 || false) {
+            this.page.remote_auth_s3 = settings.disqus.remote_auth_s3;
+          }
+          if (settings.disqus.api_key || false) {
+            this.page.api_key = settings.disqus.api_key;
+          }
+          if (settings.disqus.sso || false) {
+            this.sso = settings.disqus.sso;
+          }
+        };
+
+        // Make the AJAX call to get the Disqus comments.
+        jQuery.ajax({
+          type: 'GET',
+          url: 'http://' + disqus_shortname + '.disqus.com/embed.js',
+          dataType: 'script',
+          cache: false
+        });
+      }
+
+      // Load the comment numbers JavaScript.
+      if (settings.disqusComments || false) {
+        disqus_shortname = settings.disqusComments;
+        // Make the AJAX call to get the number of comments.
+        jQuery.ajax({
+          type: 'GET',
+          url: 'http://' + disqus_shortname + '.disqus.com/count.js',
+          dataType: 'script',
+          cache: false
+        });
+      }
+    });
+  }
+};
+
+})(jQuery);
+;
+(function ($) {
+
+$(document).ready(function() {
+
+  // Accepts a string; returns the string with regex metacharacters escaped. The returned string
+  // can safely be used at any point within a regex to match the provided literal string. Escaped
+  // characters are [ ] { } ( ) * + ? - . , \ ^ $ # and whitespace. The character | is excluded
+  // in this function as it's used to separate the domains names.
+  RegExp.escapeDomains = function(text) {
+    return (text) ? text.replace(/[-[\]{}()*+?.,\\^$#\s]/g, "\\$&") : '';
+  }
+
+  // Attach onclick event to document only and catch clicks on all elements.
+  $(document.body).click(function(event) {
+    // Catch the closest surrounding link of a clicked element.
+    $(event.target).closest("a,area").each(function() {
+
+      var ga = Drupal.settings.googleanalytics;
+      // Expression to check for absolute internal links.
+      var isInternal = new RegExp("^(https?):\/\/" + window.location.host, "i");
+      // Expression to check for special links like gotwo.module /go/* links.
+      var isInternalSpecial = new RegExp("(\/go\/.*)$", "i");
+      // Expression to check for download links.
+      var isDownload = new RegExp("\\.(" + ga.trackDownloadExtensions + ")$", "i");
+      // Expression to check for the sites cross domains.
+      var isCrossDomain = new RegExp("^(https?|ftp|news|nntp|telnet|irc|ssh|sftp|webcal):\/\/.*(" + RegExp.escapeDomains(ga.trackCrossDomains) + ")", "i");
+
+      // Is the clicked URL internal?
+      if (isInternal.test(this.href)) {
+        // Is download tracking activated and the file extension configured for download tracking?
+        if (ga.trackDownload && isDownload.test(this.href)) {
+          // Download link clicked.
+          var extension = isDownload.exec(this.href);
+          _gaq.push(["_trackEvent", "Downloads", extension[1].toUpperCase(), this.href.replace(isInternal, '')]);
+        }
+        else if (isInternalSpecial.test(this.href)) {
+          // Keep the internal URL for Google Analytics website overlay intact.
+          _gaq.push(["_trackPageview", this.href.replace(isInternal, '')]);
+        }
+      }
+      else {
+        if (ga.trackMailto && $(this).is("a[href^=mailto:],area[href^=mailto:]")) {
+          // Mailto link clicked.
+          _gaq.push(["_trackEvent", "Mails", "Click", this.href.substring(7)]);
+        }
+        else if (ga.trackOutbound && this.href) {
+          if (ga.trackDomainMode == 2 && isCrossDomain.test(this.href)) {
+            // Top-level cross domain clicked. document.location is handled by _link internally.
+            _gaq.push(["_link", this.href]);
+          }
+          else if (ga.trackOutboundAsPageview) {
+            // Track all external links as page views after URL cleanup.
+            // Currently required, if click should be tracked as goal.
+            _gaq.push(["_trackPageview", '/outbound/' + this.href.replace(/^(https?|ftp|news|nntp|telnet|irc|ssh|sftp|webcal):\/\//i, '').split('/').join('--')]);
+          }
+          else {
+            // External link clicked.
+            _gaq.push(["_trackEvent", "Outbound links", "Click", this.href]);
+          }
+        }
+      }
+    });
+  });
+});
+
+})(jQuery);
+;
